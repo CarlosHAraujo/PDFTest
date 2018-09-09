074de261fb00c847c812caac6c247dfaac79fad6
Carlos Araujo <carlinhos10002@hotmail.com>
2018-09-10 01:20:01 +0200

    final test

--- PDFTest/Program.cs
+++ PDFTest/Program.cs
@@ -1,38 +1,37 @@
-﻿using PdfSharp.Pdf.AcroForms;
-using System;
-using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using iTextSharp.text.pdf;
 using System.IO;
-using System.Linq;
-using PdfSharp.Pdf;
+using iTextSharp.text;
 
 namespace PDFTest
 {
     public static class FieldNames
     {
-        public static (string, Type) WebSite = ("website", typeof(PdfTextField));
-        public static (string, Type) PdfSubmission = ("pdf_submission", typeof(PdfTextField));
-        public static (string, Type) SimpleSpc = ("simple_spc", typeof(PdfTextField));
-        public static (string, Type) FirstName = ("q3_name[first]", typeof(PdfTextField));
-        public static (string, Type) LastName = ("q3_name[last]", typeof(PdfTextField));
-        public static (string, Type) Email = ("q4_email", typeof(PdfTextField));
-        public static (string, Type) AddressLine1 = ("q5_address[addr_line1]", typeof(PdfTextField));
-        public static (string, Type) AddressLine2 = ("q5_address[addr_line2]", typeof(PdfTextField));
-        public static (string, Type) AddressCity = ("q5_address[city]", typeof(PdfTextField));
-        public static (string, Type) AddressState = ("q5_address[state]", typeof(PdfTextField));
-        public static (string, Type) AddressPostal = ("q5_address[postal]", typeof(PdfTextField));
-        public static (string, Type) PhoneArea = ("q6_phoneNumber[area]", typeof(PdfTextField));
-        public static (string, Type) Phone = ("q6_phoneNumber[phone]", typeof(PdfTextField));
-        public static (string, Type) Date = ("q7_date", typeof(PdfTextField));
-        public static (string, Type) TimeHour = ("q8_time[hourSelect]", typeof(PdfComboBoxField));
-        public static (string, Type) TimeMinute = ("q8_time[minuteSelect]", typeof(PdfComboBoxField));
-        public static (string, Type) TimeAmPm = ("q8_time[ampm]", typeof(PdfComboBoxField));
-        public static (string, Type) ThisIs = ("q9_thisIs", typeof(PdfComboBoxField));
-        public static (string, Type) ThisIs10 = ("q10_thisIs10", typeof(PdfRadioButtonField));
-        public static (string, Type) ThisIsOption1 = ("q11_thisIs11[Type option 1]", typeof(PdfCheckBoxField));
-        public static (string, Type) ThisIsOption2 = ("q11_thisIs11[Type option 2]", typeof(PdfCheckBoxField));
-        public static (string, Type) ThisIsOption3 = ("q11_thisIs11[Type option 3]", typeof(PdfCheckBoxField));
-        public static (string, Type) ThisIsOption4 = ("q11_thisIs11[Type option 4]", typeof(PdfCheckBoxField));
-        public static (string, Type) Submit = ("Submit", typeof(PdfPushButtonField));
+        public static string WebSite = "website";
+        public static string PdfSubmission = "pdf_submission";
+        public static string SimpleSpc = "simple_spc";
+        public static string FirstName = "q3_name[first]";
+        public static string LastName = "q3_name[last]";
+        public static string Email = "q4_email";
+        public static string AddressLine1 = "q5_address[addr_line1]";
+        public static string AddressLine2 = "q5_address[addr_line2]";
+        public static string AddressCity = "q5_address[city]";
+        public static string AddressState = "q5_address[state]";
+        public static string AddressPostal = "q5_address[postal]";
+        public static string PhoneArea = "q6_phoneNumber[area]";
+        public static string Phone = "q6_phoneNumber[phone]";
+        public static string Date = "q7_date";
+        public static string TimeHour = "q8_time[hourSelect]";
+        public static string TimeMinute = "q8_time[minuteSelect]";
+        public static string TimeAmPm = "q8_time[ampm]";
+        public static string ThisIs = "q9_thisIs";
+        public static string ThisIs10 = "q10_thisIs10";
+        public static string ThisIsOption1 = "q11_thisIs11[Type option 1]";
+        public static string ThisIsOption2 = "q11_thisIs11[Type option 2]";
+        public static string ThisIsOption3 = "q11_thisIs11[Type option 3]";
+        public static string ThisIsOption4 = "q11_thisIs11[Type option 4]";
+        public static string Submit = "Submit";
     }
 
 
@@ -41,48 +40,110 @@ namespace PDFTest
         static void Main(string[] args)
         {
             string currentPath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
-            var document = PdfSharp.Pdf.IO.PdfReader.Open($"{currentPath}\\teste.pdf", PdfSharp.Pdf.IO.PdfDocumentOpenMode.Modify);
+            string template = $"{currentPath}\\teste.pdf";
+            string finalArchive = $"{currentPath}\\final.pdf";
 
-            var form = document.AcroForm;
 
-            if (form.Elements.ContainsKey("/NeedAppearances"))
+            //using (var finalFile = new FileStream(finalArchive, FileMode.Create, FileAccess.Write, FileShare.None))
+            //{
+            //    var reader = new PdfReader(template);
+
+            //    var copy = new PdfCopyFields(finalFile, PdfWriter.VERSION_1_4);
+            //    copy.AddDocument(reader);
+            //    copy.Close();
+            //}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            using (var stream = new FileStream(finalArchive, FileMode.Create, FileAccess.Write, FileShare.None))
             {
-                form.Elements["/NeedAppearances"] = new PdfBoolean(true);
-            }
-            else
-            {
-                form.Elements.Add("/NeedAppearances", new PdfBoolean(true));
+                var reader = new PdfReader(template);
+
+                var stamper = new PdfStamper(reader, stream);
+
+                var fields = stamper.AcroFields;
+
+                fields.SetField(FieldNames.AddressCity, "Sete Lagoas");
+
+                //var item = fields.GetFieldItem(FieldNames.AddressCity);
+                //item.values = new ArrayList() { "Sete Lagoas" };
+                //fields.Fields[FieldNames.AddressCity] = item;
+
+                stamper.FormFlattening = true;
+
+                stamper.AcroFields.GenerateAppearances = true;
+                
+                reader.Close();
+                stamper.Close();
             }
 
-            var timeHour = (PdfComboBoxField)form.Fields[FieldNames.TimeHour.Item1];
-            timeHour.SelectedIndex = 5;
-            timeHour.ReadOnly = true;
-            //timeHour.SelectedIndex = 1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //if (form.Elements.ContainsKey("/NeedAppearances"))
+            //{
+            //    form.Elements["/NeedAppearances"] = new PdfBoolean(true);
+            //}
+            //else
+            //{
+            //    form.Elements.Add("/NeedAppearances", new PdfBoolean(true));
+            //}
+
+            //var timeHour = form.Fields
+            //timeHour.SelectedIndex = 5;
             //timeHour.ReadOnly = true;
+            ////timeHour.SelectedIndex = 1;
+            ////timeHour.ReadOnly = true;
 
-            var timeMinute = (PdfComboBoxField)form.Fields[FieldNames.TimeMinute.Item1];
-            timeMinute.SelectedIndex = 1;
-            //timeMinute.ReadOnly = true;
+            //var timeMinute = (PdfComboBoxField)form.Fields[FieldNames.TimeMinute.Item1];
+            //timeMinute.SelectedIndex = 1;
+            ////timeMinute.ReadOnly = true;
 
-            var timeAmPm = (PdfComboBoxField)form.Fields[FieldNames.TimeAmPm.Item1];
-            timeAmPm.SelectedIndex = 1;
-            //timeAmPm.ReadOnly = true;
+            //var timeAmPm = (PdfComboBoxField)form.Fields[FieldNames.TimeAmPm.Item1];
+            //timeAmPm.SelectedIndex = 1;
+            ////timeAmPm.ReadOnly = true;
 
             //var field = (PdfTextField)form.Fields["q3_name[first]"];
             //field.Value = new PdfSharp.Pdf.PdfString("Carlos");
             //field.ReadOnly = true;
 
-            document.Save($"{currentPath}\\teste2.pdf");
-
-            document.Close();
+            //document.Save($"{currentPath}\\teste2.pdf");
 
             Console.ReadKey();
-        }
-
-        private static void PrintFieldInfo(PdfAcroForm form, string key)
-        {
-            var addressCity = form.Fields[key];
-            Console.WriteLine($"{key} typeof = {addressCity.GetType()}");
         }
     }
 }
